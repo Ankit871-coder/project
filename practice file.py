a63b018581021d6d5818b63190291b0e0c6926e8
Ankit871-coder <mr.ankitk2000@gmail.com>
2025-05-05 11:24:49 +0530

    Add files via upload

---
+++ practice file.py
@@ -0,0 +1,15 @@
+'''
+print("ankit kumar\nrahul prajapat\nashish meena\najay gwariya")
+print("ankit",10,10.0,10j,sep=" , ")
+print("ankit",10,10j,end="11")
+print("kumar")
+print("pajapat")
+'''
+
+a=list("abcd")
+print(a)
+
+for i in "ankit":
+    if(i=="k"):
+        continue
+    print(i)
